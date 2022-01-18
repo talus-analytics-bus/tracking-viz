21798456b2c6820ec020380c1ce9f0986f1c6b2b
mvanmaele <mvanmaele@talusanalytics.com>
2022-01-18 10:57:59 -0500

    Refactor data for map

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,13 @@ import Table from "./components/Table/Table";
 import tableData from "./components/Table/topfunders.json";
 import ColorMap from "./components/Map/ColorMap/ColorMap";
 
+// data
+import unmetneeds from "./components/Map/unmetneeds.json";
+
 function App() {
   return (
     <div className="App">
-      <ColorMap />
+      <ColorMap data={unmetneeds} />
       {/* <Table data={tableData} /> */}
       {/* <FundingSankey /> */}
       {/* <JEETreemap /> */}
--- src/components/Map/ColorMap/ColorMap.tsx
+++ src/components/Map/ColorMap/ColorMap.tsx
@@ -1,8 +1,9 @@
 import Map from "../Map";
-import data from "../unmetneeds.json";
 
-const colors: string[] = [...new Set(data.map((d: any) => d.Color as string))];
-export const ColorMap = () => {
+export const ColorMap = ({ data }: { data: any[] }) => {
+  const colors: string[] = [
+    ...new Set(data.map((d: any) => d.Color as string)),
+  ];
   return (
     <Map
       id={(d: any) => d["Stakeholder ISO3"]}
