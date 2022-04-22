996c687cfdb878cb1a9ea15b652c5cf02d9ddbb5
mvanmaele <mvanmaele@talusanalytics.com>
2022-04-22 15:05:55 -0400

    Style nav links

--- src/layout/Nav.tsx
+++ src/layout/Nav.tsx
@@ -1,11 +1,36 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import styled from 'styled-components'
+
+const NavLinkButtonContainer = styled.button`
+    a {
+        text-decoration: none;
+        &.active {
+            font-weight: bold;
+        }
+    }
+`
+
+function NavLinkButton(props: any) {
+    return (
+        <NavLinkButtonContainer>
+            <NavLink to={props.link.path}>{props.link.label}</NavLink>
+        </NavLinkButtonContainer>
+    )
+}
 
 export const Nav = () => {
-    const links = [{ path: 'maps', label: 'Maps' }, { path: 'jeeTreemap', label: 'JEE Treemap' },{ path: 'tables', label: 'Tables' },{ path: 'sankey', label: 'Sankey' }]
+    const links = [
+        { path: 'maps', label: 'Maps' },
+        { path: 'jeeTreemap', label: 'JEE Treemap' },
+        { path: 'tables', label: 'Tables' },
+        { path: 'sankey', label: 'Sankey' },
+    ]
     return (
-        <ul>{links.map(link => <button>
-            <NavLink to={link.path}>{link.label}</NavLink>
-        </button>)}</ul>
+        <ul>
+            {links.map((link) => (
+                <NavLinkButton link={link}></NavLinkButton>
+            ))}
+        </ul>
     )
 }
